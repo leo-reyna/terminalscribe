110ba40a90133c1941ecfadc46eca4a89ba5ee54
LReyna <leo.reyna@gmail.com>
2023-01-26 21:23:26 -0500

    defining class canvas

--- terminalscribe.py
+++ terminalscribe.py
@@ -8,4 +8,11 @@ from termcolor import colored
 
 os.system('cls')
 
-
+class Canvas:
+    def __init__(self, width, height) -> None:
+        pass
+        self._x = width
+        self._y = height
+        self._canvas = [[' ' for y in range(self._y) for x in range(self._x)]]
+    def hitsWall(self,point):
+        return point[0] < 0 or point[0] >= self._x or point[1]
